44d8ac963ce3a80c2ee46bd780d290faaedab1ca
Brian Corbin <me@briancorbin.co.uk>
2024-09-04 23:40:22 -0400

    broken: Attempt to await two players

--- tictactoe-game-service/src/main/java/org/xxdc/oss/example/GameService.java
+++ tictactoe-game-service/src/main/java/org/xxdc/oss/example/GameService.java
@@ -1,6 +1,9 @@
 package org.xxdc.oss.example;
 
 import org.xxdc.oss.example.service.TicTacToeGame;
+
+import java.util.List;
+
 import org.xxdc.oss.example.service.GameMoveRequest;
 import org.xxdc.oss.example.service.GameUpdate;
 import org.xxdc.oss.example.service.JoinRequest;
@@ -9,33 +12,58 @@ import org.xxdc.oss.example.service.JoinResponse;
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.operators.multi.processors.BroadcastProcessor;
 
 @GrpcService
 public class GameService implements TicTacToeGame {
 
     private final GameManager gameManager = new GameManager();
 
+    static class GameManager {
+
+        private final BroadcastProcessor<JoinRequest> joinRequests = BroadcastProcessor.create();
+
+        public GameManager() {
+            // activate the processor so that it starts processing events
+            joinRequests.log().subscribe().with(request -> {
+                System.out.println("Received join request: " + request);
+            });
+        }
+        
+        public void addJoinRequest(JoinRequest request) {
+            joinRequests.onNext(request);
+        }
+
+        public Multi<JoinRequest> joinRequests() {
+            return joinRequests;
+        }
+    }
+
     @Override
     public Uni<JoinResponse> joinGame(JoinRequest request) {
+        Uni<List<JoinRequest>> waitForMinimumPlayers = gameManager.joinRequests()
+            .select().first(2)
+            .collect().asList()
+            .log();
+
+        var response = waitForMinimumPlayers.onItem().transform(players -> {
+            System.out.println("Starting game with players: " + players);
+            return JoinResponse.newBuilder()
+                //.setMessage("Welcome " + request.getName() + "!")
+                .setMessage("Welcomed " + players.stream().map(JoinRequest::getName).reduce("", (a, b) -> a + b) + "!")
+                .build();
+        }).log().subscribe().with(players -> {
+            System.out.println("Received response: " + players);
+        });
         gameManager.addJoinRequest(request);
-        return Uni.createFrom()
-            .item(JoinResponse.newBuilder()
-                .setMessage("Welcome " + request.getName() + "!").build()
-            );
+        return response;
     }
+
+
 
     @Override
     public Uni<GameUpdate> makeMove(GameMoveRequest request) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
     }
-
-    static class GameManager {
-        private final Multi<JoinRequest> joinRequests = Multi.createFrom().empty();
-
-        public void addJoinRequest(JoinRequest request) {
-            //joinRequests.emit(request);
-        }
-    }
-
 }
--- tictactoe-game-service/src/test/java/org/xxdc/oss/example/GameServiceTest.java
+++ tictactoe-game-service/src/test/java/org/xxdc/oss/example/GameServiceTest.java
@@ -21,17 +21,27 @@ public class GameServiceTest {
 
     @Test
     public void testJoinGame_SuccessfulJoin() {
-        JoinRequest request = JoinRequest.newBuilder().setName("Player1").build();
-        Uni<JoinResponse> response = gameService.joinGame(request);
+        var request = JoinRequest.newBuilder().setName("Player1").build();
+        var requestTwo = JoinRequest.newBuilder().setName("Player2").build();
+        var response = gameService.joinGame(request);
+        var responseTwo = gameService.joinGame(requestTwo);
         response.subscribe().with(joinResponse -> {
+            System.out.println("Received response: " + joinResponse.getMessage());
+            assertEquals("Welcome Player1!", joinResponse.getMessage());
+        },
+        error -> {
+            System.out.println("Error: " + error.getMessage());
+        });
+        responseTwo.subscribe().with(joinResponse -> {
+            System.out.println("Received response: " + joinResponse.getMessage());
             assertEquals("Welcome Player1!", joinResponse.getMessage());
         });
     }
 
     @Test
     public void testJoinGame_EmptyMessage() {
-        JoinRequest request = JoinRequest.newBuilder().setName("").build();
-        Uni<JoinResponse> response = gameService.joinGame(request);
+        var request = JoinRequest.newBuilder().setName("").build();
+        var response = gameService.joinGame(request);
         response.subscribe().with(joinResponse -> {
             assertEquals("Welcome !", joinResponse.getMessage());
         });
@@ -39,8 +49,8 @@ public class GameServiceTest {
 
     @Test
     public void testJoinGame_NullMessage() {
-        JoinRequest request = JoinRequest.newBuilder().build();
-        Uni<JoinResponse> response = gameService.joinGame(request);
+        var request = JoinRequest.newBuilder().build();
+        var response = gameService.joinGame(request);
         response.subscribe().with(joinResponse -> {
             assertEquals("Welcome null!", joinResponse.getMessage());
         });
