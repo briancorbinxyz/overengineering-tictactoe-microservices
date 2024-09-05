7317396376deee4ce7034ea62e8a307e9d1eb7ed
Brian Corbin <me@briancorbin.co.uk>
2024-09-04 21:44:52 -0400

    feat: Initial game service protocol

--- tictactoe-game-service/src/main/java/org/xxdc/oss/example/GameService.java
+++ tictactoe-game-service/src/main/java/org/xxdc/oss/example/GameService.java
@@ -1,19 +1,41 @@
 package org.xxdc.oss.example;
 
-import org.xxdc.oss.example.service.Game;
-import org.xxdc.oss.example.service.JoinReply;
+import org.xxdc.oss.example.service.TicTacToeGame;
+import org.xxdc.oss.example.service.GameMoveRequest;
+import org.xxdc.oss.example.service.GameUpdate;
 import org.xxdc.oss.example.service.JoinRequest;
+import org.xxdc.oss.example.service.JoinResponse;
 
 import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 @GrpcService
-public class GameService implements Game {
+public class GameService implements TicTacToeGame {
+
+    private final GameManager gameManager = new GameManager();
 
     @Override
-    public Uni<JoinReply> joinGame(JoinRequest request) {
-        return Uni.createFrom().item(JoinReply.newBuilder().setMessage("Hello " + request.getMessage()).build());
+    public Uni<JoinResponse> joinGame(JoinRequest request) {
+        gameManager.addJoinRequest(request);
+        return Uni.createFrom()
+            .item(JoinResponse.newBuilder()
+                .setMessage("Hello " + request.getMessage()).build()
+            );
+    }
 
+    @Override
+    public Uni<GameUpdate> makeMove(GameMoveRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
+    }
+
+    static class GameManager {
+        private final Multi<JoinRequest> joinRequests = Multi.createFrom().empty();
+
+        public void addJoinRequest(JoinRequest request) {
+            //joinRequests.emit(request);
+        }
     }
 
 }
