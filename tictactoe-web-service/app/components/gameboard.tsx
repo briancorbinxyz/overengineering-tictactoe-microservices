5d52b763c93824c47be5d50cdf1d9a7c01c0b255
Brian Corbin <me@briancorbin.co.uk>
2024-11-09 13:23:37 -0500

    feature(web-service): Let's have some audio

--- tictactoe-web-service/app/components/gameboard.tsx
+++ tictactoe-web-service/app/components/gameboard.tsx
@@ -1,10 +1,23 @@
 import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 import invariant from "tiny-invariant";
 import type { GameState } from "~/models/game";
 import { makeMove } from "~/services/game.api";
+import moveAudioUrl from '~/audio/bubble-pop-ding-betacut-1-00-01.mp3';
+import gameAudioUrl from '~/audio/itty-bitty-8-bit-kevin-macleod-main-version-7983-03-13.mp3';
 
 const GameBoard = ({ state, gameId, activePlayerId }: GameState) => {
   const { dimension, contents } = state.board;
+  const [moveAudio, setMoveAudio] = useState<HTMLAudioElement>();
+  const [gameAudio, setGameAudio] = useState<HTMLAudioElement>();
+
+  // Set up audio
+  useEffect(() => {
+      // Audio is only available on the browser (client) side
+      setMoveAudio(new Audio(moveAudioUrl));
+      setGameAudio(new Audio(gameAudioUrl));
+      gameAudio?.pause();
+  }, []);
 
   // Break the contents array into rows based on the board's dimension
   const rows = Array.from({ length: dimension }, (_, rowIndex) => {
@@ -22,9 +35,10 @@ const GameBoard = ({ state, gameId, activePlayerId }: GameState) => {
     );
 
     // Only allow the active player to make a move
-    if (gameId && activePlayerId && !state.completed) {
+    if (gameId && activePlayerId) {
       if (state.current_player_index == activePlayerId.index) {
         console.log("Move selected", locationId);
+        await moveAudio?.play();
         await makeMove(gameId, locationId, activePlayerId?.marker);
       } else {
         // TODO: Make more visible
@@ -32,18 +46,6 @@ const GameBoard = ({ state, gameId, activePlayerId }: GameState) => {
       }
     } else {
       console.debug("Clicked", locationId);
-    }
-  };
-
-  const gameStatus = () => {
-    if (state.completed) {
-      if (state.winning_player_index !== undefined) {
-        return `Player ${state.players[state.winning_player_index].marker} wins!`;
-      } else {
-        return "It's a draw!";
-      }
-    } else {
-      return `Turn: ${state.players[state.current_player_index].marker}`;
     }
   };
 
@@ -55,7 +57,7 @@ const GameBoard = ({ state, gameId, activePlayerId }: GameState) => {
             {row.map((marker, markerIndex) => (
               <motion.button
                 whileHover={{ scale: 1.15 }}
-                className={`flex h-14 w-14 ${marker === activePlayerId?.marker ? "text-blue-900" : ""} items-center justify-center border-opacity-45 bg-yellow-700 bg-opacity-20 font-['Strong_Young'] text-6xl dark:border-gray-50`}
+                className={`flex h-14 w-14 ${marker === activePlayerId?.marker ? 'text-blue-900' : ''} items-center justify-center border-opacity-45 bg-yellow-700 bg-opacity-20 font-['Strong_Young'] text-6xl dark:border-gray-50`}
                 key={markerIndex}
                 onClick={handleClick}
                 id={String(rowIndex * dimension + markerIndex)}
@@ -69,9 +71,7 @@ const GameBoard = ({ state, gameId, activePlayerId }: GameState) => {
           </div>
         ))}
       </div>
-      <div className="flex justify-center font-['Strong_Young'] text-black">
-        {gameId ? gameStatus() : ""}
-      </div>
+      <div className="flex justify-center font-['Strong_Young'] text-black">{gameId ? 'Turn: ' + state.players[state.current_player_index].marker : ''}</div>
     </div>
   );
 };
--- tictactoe-web-service/app/routes/games.tsx
+++ tictactoe-web-service/app/routes/games.tsx
@@ -62,9 +62,9 @@ const Game = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"
             />
           </svg>
@@ -95,7 +95,7 @@ const Game = () => {
                   type="submit"
                   disabled={!isIdle}
                 >
-                  {isIdle ? "Join Game" : "Joining Game..."}
+                  {isIdle ? "Join Game" : "Matchmaking..."}
                 </motion.button>
               </div>
             </Form>
